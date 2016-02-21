1c2a2dbd6ca32e5f4a6f37d0ed275c4916e0fe66
Marcus Paulo <mpaulobr@gmail.com>
2016-02-21 09:50:47 -0300

    Ajuste na Tela de Detalhes

--- AgendaCultural/Controllers/DetalheEventoViewController.swift
+++ AgendaCultural/Controllers/DetalheEventoViewController.swift
@@ -15,10 +15,21 @@ class DetalheEventoViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var openingDateLabel: UILabel!
+    @IBOutlet weak var endingDateLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     
     override func viewDidLoad() {
         titleLabel.text = evento.title
         placeLabel.text = evento.place
         priceLabel.text = evento.price
+        
+        descriptionLabel.text = evento.description
+        addressLabel.text = evento.address
+        openingDateLabel.text = evento.opening_date
+        endingDateLabel.text = evento.ending_date
+        categoryLabel.text = evento.category
     }
 }
